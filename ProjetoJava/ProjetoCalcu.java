f9004d032254a6bd40c1e6d3f71a16c9fd22ba49
Lincoln <raposaasilvaa@gmail.com>
2020-09-02 14:18:02 -0300

    Projeto Java

---
+++ ProjetoJava/ProjetoCalcu.java
@@ -0,0 +1,13 @@
+public class ProjetoCalcu
+{
+    public static void main (String[] args){
+        
+        double valorSalario = 1300.45;
+        int numeroDependentes = 5;
+        String nomeCidade = "Bauru";      
+        char letra = 'A';
+        boolean temSaldo = true;
+        
+        System.out.println("Meu segundo código de Java na Fib");
+   }
+}
---
+++ ProjetoJava/SomaModificado.java
@@ -0,0 +1,17 @@
+public class SomaModificado
+{
+    public static void main (String[] args){
+        
+        double valorSalario = 1300.45;
+        int numeroDependentes = 5;
+        String nomeCidade = "Bauru";      
+        char letra = 'A';       
+        boolean temSaldo = true;
+      
+        double valor1 = 10.20;
+        double valor2 = 30.20;
+        double soma = valor1 + valor2;
+        
+        System.out.println("A soma de " + valor1 +" mais " + valor2 + " é " + soma);
+    }
+}
